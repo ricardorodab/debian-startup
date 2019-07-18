b3f2ee219a2675307ef3de918fdbd0090ba9e868
Ricardo Abreu <ricardorodab19@gmail.com>
2019-07-17 23:50:21 -0500

    Docker-compose

--- asusZenbook/docker.sh
+++ asusZenbook/docker.sh
@@ -15,3 +15,8 @@ add-apt-repository \
 apt-get update
 apt-get install docker-ce docker-ce-cli containerd.io
 apt-get install docker-compose
+groupadd docker
+NOW_USER=$(last -p now | cut -d ":" -f 1 | tr -d " " | cut -f 1 | cut -d$'\n' -f1)
+usermod -aG docker $NOW_USER
+unset NOW_USER
+
